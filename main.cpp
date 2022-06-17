db49e6f046577869d5817b3bb35d72a1ca166fb9
SubwayMan <tyche4490@gmail.com>
2022-06-16 18:08:34 -0700

    basic bouncing ball

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
+#include <iostream>
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
 class Ball {
     public:
         sf::CircleShape C;
         Ball(int rad, int x, int y, int r, int g, int b) {
-            C.setRadius(r);
+            C.setRadius(rad);
             C.setPosition(x, y);
             C.setFillColor(sf::Color(r, g, b));
         }
-        void move(float x, float y);
+        void move(float x, float y) {
+            sf::Vector2f mv(x, y);
+            C.setPosition(C.getPosition() + mv);
+        }
+
         void update();
 };
 
@@ -22,17 +28,30 @@ int main() {
     // Ball ball(50, 100, 30, 250, 0, 0);
     sf::Event event;
 
-    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML Test");
-    Ball b(100, 50, 50, 121, 54, 12);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
+    window.setFramerateLimit(60);
+    Ball b(30, 50, 50, 121, 54, 12);
+
+    int xVel = 3;
+    int yVel = 3;
+    float brad = b.C.getRadius();
 
     while (window.isOpen()) {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        window.clear(sf::Color::Blue);
+        window.clear(sf::Color::Black);
         // display code here
         window.draw(b.C);
+
+        b.move(xVel, yVel);
+        sf::Vector2f bpos = b.C.getPosition();
+        if (bpos.x + 2*brad > 800) xVel = abs(xVel) * -1;
+        if (bpos.y + 2*brad> 600) yVel = abs(yVel) * -1;
+        if (bpos.x <= 0) xVel = abs(xVel);
+        if (bpos.y <= 0) yVel = abs(yVel);
+    
 
         window.display();
     }
